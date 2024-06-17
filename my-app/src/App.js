80eb26a9290d19bfbb09911ef2af512bda3e65e9
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2024-06-17 13:21:35 +0000

    The useEffect() Hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import UseStateWithObjects  from './components/UseStateWithObjects'
+import UseEffectCounter from './components/UseEffectCounter'
 
 function App() {
   return <div className="App">
-      <UseStateWithObjects />
+      <UseEffectCounter />
     </div>;
 }
 
